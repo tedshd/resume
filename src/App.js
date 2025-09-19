064ec74e06145f49b12c4c5161971a5cad6cb918
tedshd <tedshd@gmail.com>
2025-09-19 10:18:18 +0800

    Refactor code structure for improved readability and maintainability

--- src/App.js
+++ src/App.js
@@ -61,7 +61,7 @@ function App() {
         </section>
         <h2>{t('introduction')}</h2>
         <article>{t('intro')}</article>
-        <section className='pure-g'>
+        <section className='pure-g print_section'>
           <div className="pure-u-1 pure-u-md-1-2">
             <h2>{t('specialty')}</h2>
             <section>
@@ -103,7 +103,7 @@ function App() {
           </div>
         </section>
         <h2>{t('skill')}</h2>
-        <section className='pure-g'>
+        <section className='pure-g print_section'>
           <div className="pure-u-1 pure-u-md-1-4">
             <h3>{t('front_end')}</h3>
             <ul>
@@ -162,49 +162,65 @@ function App() {
           </div>
         </section>
         <h2>{t('record')}</h2>
-        <section>
-          <h3>2013</h3>
-          <article>
-            <p>Participate in Yahoo Hack Taiwan 2013</p>
-            <p><a href="https://www.youtube.com/watch?v=KfV3ZbfyaMc" target="_blank" rel="noreferrer">demo</a></p>
-          </article>
-          <h3>2014</h3>
-          <article>
-            <p>Participate in 2014 Samsung App develop game(The second place)(Best Smart TV develop)</p>
-            <p><a href="http://edm.bnext.com.tw/2014samsungdeveloper/about_app_content.html" target="_blank" rel="noreferrer">Info</a></p>
-            <p><a href="https://www.youtube.com/watch?v=f1T9q5V3s48" target="_blank" rel="noreferrer">demo</a></p>
-            <p>Been invited be a FED Party 12 lecturer</p>
-            <p><a href="https://www.youtube.com/watch?v=vr5HtuNX4p4" target="_blank" rel="noreferrer">YouTube</a></p>
-            <p><a href="https://tedshd.github.io/FED_Party_12/" target="_blank" rel="noreferrer">demo</a></p>
-          </article>
-          <h3>2015</h3>
-          <article>
-            <p>Participate in 2015 Hsinchu hackathon(champion)</p>
-            <img className="pure-img" src="https://c3.staticflickr.com/9/8353/30009083466_6203d7c9d0_c.jpg" alt="2015 Hsinchu hackathon" />
-            <p><a href="https://www.flickr.com/gp/78340210@N04/1hyaa5" target="_blank" rel="noreferrer">Design</a></p>
-            <p><a href="https://tedshd.io/2015_hsinchu_hackthon/" target="_blank" rel="noreferrer">demo</a></p>
-          </article>
-          <h3>2016</h3>
-          <article>
-            <p>Participate in 一秒搞懂政府網站創意競賽(通觀全局獎)</p>
-            <img className="pure-img" src="https://c7.staticflickr.com/9/8737/28080293190_e26d880d9f_c.jpg" alt="一秒搞懂政府網站創意競賽" />
-            <p>Share firebase with WTT #10</p>
-            <p><a href="https://www.youtube.com/watch?v=ppyBS2Hbf4A" target="_blank" rel="noreferrer">Video</a></p>
-            <p><a href="https://docs.google.com/presentation/d/1we7yKYl1FtvmLaLdrLHml1j49LCd5qIVC8F04hyjqtM/edit?usp=sharing" target="_blank" rel="noreferrer">Firebase slide share</a></p>
-          </article>
-          <h3>2017</h3>
-          <article>
-            <p>Share 了解 JavaScript 記憶體管理來調整效能與畫面入門 with Node 線上讀書會</p>
-            <p><a href="https://www.youtube.com/watch?v=lNqJDqycSEo" target="_blank" rel="noreferrer">Video</a></p>
-            <p><a href="https://hackmd.io/p/ByoQ6xWfZ#/" target="_blank" rel="noreferrer">slide share</a></p>
-            <p>資料應用黑客松－嘉義黑蚵松(最佳人氣獎)</p>
-            <img src="https://farm5.staticflickr.com/4419/37197944845_ef8138c855_b.jpg" alt="嘉義黑蚵松" className="pure-img" />
-          </article>
-          <h3>2018</h3>
-          <article>
-            <p>FEDC 2018 Front-End Workshop 講者</p>
-            <p><a href="http://2018.fedc.tw/" target="_blank" rel="noreferrer">FEDC 2018 - Front-End x Design Conference</a></p>
-          </article>
+        <section className="print_section">
+          <div className="print_record">
+            <h3>2013</h3>
+            <article>
+              <p>Participate in Yahoo Hack Taiwan 2013</p>
+              <p><a href="https://www.youtube.com/watch?v=KfV3ZbfyaMc" target="_blank" rel="noreferrer">demo</a></p>
+            </article>
+          </div>
+          <div className="print_record">
+            <h3>2014</h3>
+            <article>
+              <p>Participate in 2014 Samsung App develop game(The second place)(Best Smart TV develop)</p>
+              <p><a href="http://edm.bnext.com.tw/2014samsungdeveloper/about_app_content.html" target="_blank" rel="noreferrer">Info</a></p>
+              <p><a href="https://www.youtube.com/watch?v=f1T9q5V3s48" target="_blank" rel="noreferrer">demo</a></p>
+              <p>Been invited be a FED Party 12 lecturer</p>
+              <p><a href="https://www.youtube.com/watch?v=vr5HtuNX4p4" target="_blank" rel="noreferrer">YouTube</a></p>
+              <p><a href="https://tedshd.github.io/FED_Party_12/" target="_blank" rel="noreferrer">demo</a></p>
+            </article>
+          </div>
+        </section>
+        <section className="print_section">
+          <div className="print_record">
+            <h3>2015</h3>
+            <article>
+              <p>Participate in 2015 Hsinchu hackathon(champion)</p>
+              <img className="pure-img" src="https://c3.staticflickr.com/9/8353/30009083466_6203d7c9d0_c.jpg" alt="2015 Hsinchu hackathon" />
+              <p><a href="https://www.flickr.com/gp/78340210@N04/1hyaa5" target="_blank" rel="noreferrer">Design</a></p>
+              <p><a href="https://tedshd.io/2015_hsinchu_hackthon/" target="_blank" rel="noreferrer">demo</a></p>
+            </article>
+          </div>
+          <div className="print_record">
+            <h3>2016</h3>
+            <article>
+              <p>Participate in 一秒搞懂政府網站創意競賽(通觀全局獎)</p>
+              <img className="pure-img" src="https://c7.staticflickr.com/9/8737/28080293190_e26d880d9f_c.jpg" alt="一秒搞懂政府網站創意競賽" />
+              <p>Share firebase with WTT #10</p>
+              <p><a href="https://www.youtube.com/watch?v=ppyBS2Hbf4A" target="_blank" rel="noreferrer">Video</a></p>
+              <p><a href="https://docs.google.com/presentation/d/1we7yKYl1FtvmLaLdrLHml1j49LCd5qIVC8F04hyjqtM/edit?usp=sharing" target="_blank" rel="noreferrer">Firebase slide share</a></p>
+            </article>
+          </div>
+        </section>
+        <section className="print_section">
+          <div className="print_record">
+            <h3>2017</h3>
+            <article>
+              <p>Share 了解 JavaScript 記憶體管理來調整效能與畫面入門 with Node 線上讀書會</p>
+              <p><a href="https://www.youtube.com/watch?v=lNqJDqycSEo" target="_blank" rel="noreferrer">Video</a></p>
+              <p><a href="https://hackmd.io/p/ByoQ6xWfZ#/" target="_blank" rel="noreferrer">slide share</a></p>
+              <p>資料應用黑客松－嘉義黑蚵松(最佳人氣獎)</p>
+              <img src="https://farm5.staticflickr.com/4419/37197944845_ef8138c855_b.jpg" alt="嘉義黑蚵松" className="pure-img" />
+            </article>
+          </div>
+          <div className="print_record">
+            <h3>2018</h3>
+            <article>
+              <p>FEDC 2018 Front-End Workshop 講者</p>
+              <p><a href="http://2018.fedc.tw/" target="_blank" rel="noreferrer">FEDC 2018 - Front-End x Design Conference</a></p>
+            </article>
+          </div>
         </section>
         <h2>{t('entertainment')}</h2>
         <section>
--- src/components/nav.js
+++ src/components/nav.js
@@ -6,35 +6,26 @@ class Nav extends React.Component {
     <nav>
       <ul>
           <li>
-              <a href="https://tedshd.io" target="_blank" title="web">Web</a>
+              <a href="https://tedshd.io" target="_blank" title="web" rel="noreferrer">Web</a>
               <span className="print">https://tedshd.io</span>
           </li>
           <li>
-              <a href="https://github.com/tedshd" target="_blank" title="GitHub">GitHub</a>
+              <a href="https://github.com/tedshd" target="_blank" title="GitHub" rel="noreferrer">GitHub</a>
               <span className="print">https://github.com/tedshd</span>
           </li>
           <li>
               <a href="https://www.linkedin.com/in/tedshd" target="_blank"
-                  title="Linkedin">Linkedin</a>
+                  title="Linkedin" rel="noreferrer">Linkedin</a>
               <span className="print">https://www.linkedin.com/in/tedshd</span>
           </li>
           <li>
-              <a href="http://blog.tedshd.io" target="_blank" title="DevelopNote">DevelopNote</a>
+              <a href="http://blog.tedshd.io" target="_blank" title="DevelopNote" rel="noreferrer">DevelopNote</a>
               <span className="print">http://blog.tedshd.io</span>
           </li>
           <li>
               <a href="http://www.slideshare.net/tysh310246" target="_blank"
-                  title="SlideShare">SlideShare</a>
+                  title="SlideShare" rel="noreferrer">SlideShare</a>
               <span className="print">http://www.slideshare.net/tysh310246</span>
-          </li>
-          <li>
-              <a href="http://codepen.io/tedshd/" target="_blank" title="CodePen">CodePen</a>
-              <span className="print">http://codepen.io/tedshd/</span>
-          </li>
-          <li>
-              <a href="https://www.facebook.com/chander.shiu" target="_blank"
-                  title="Facebook">Facebook</a>
-              <span className="print">https://www.facebook.com/chander.shiu</span>
           </li>
       </ul>
     </nav>
