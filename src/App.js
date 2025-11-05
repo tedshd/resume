6df070babb9e3968f055dcba97326c509ee6dab9
tedshd <tedshd@gmail.com>
2025-11-05 10:07:50 +0800

    Update.

--- src/App.js
+++ src/App.js
@@ -108,6 +108,9 @@ function App() {
                 <li>
                   <a href="https://program.tex.com.tw/" target="_blank" rel="noreferrer">TEX programmable mechanical keyboard web configurator</a>
                 </li>
+                <li>
+                  <a href="http://gatherup.tw/" target="_blank" rel="noreferrer">GatherUp - 台灣車聚活動平台</a>
+                </li>
               </ul>
             </section>
           </div>
